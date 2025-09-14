faef950efb468fca29a37b62a9a9315a601711b9
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-14 22:45:04 +0530

    Added validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10000
+
+  validates :title, length: { maximum: MAX_TITLE_LENGTH }, presence: true
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }, presence: true
+  validates :is_bloggable, inclusion: { in: [true, false] }
 end
