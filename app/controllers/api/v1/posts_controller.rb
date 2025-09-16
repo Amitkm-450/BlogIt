bfbf87be75f0e15f6e1e9cebebfd9d41dd31fa5a
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-16 12:37:04 +0530

    Added show action to Post controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,23 +1,35 @@
 # frozen_string_literal: true
 
 class Api::V1::PostsController < ApplicationController
+  before_action :load_post!, only: %i[show destroy]
+
   def index
     posts = Post.all
     render json: posts
   end
 
   def create
-    puts "=============== Inside the Post::Create ================"
     post = Post.new(post_params)
-    puts "=============== Validate the Post::Create ================"
-    puts "#{post.valid?}"
     post.save!
     render_notice("Post was successfully created")
+  end
+
+  def show
+    render_json({ post: @post })
+  end
+
+  def destroy
+    @post.destroy
+    render_notice("Post was successfully deleted")
   end
 
   private
 
     def post_params
       params.require(:post).permit(:title, :description)
+    end
+
+    def load_post!
+      @post = Post.find_by!(slug: params[:slug])
     end
 end
