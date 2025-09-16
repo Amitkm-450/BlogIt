4d639f4398f215f9bc311fec227b28be50e0e189
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-16 15:55:41 +0530

    Added has_secure_password and validations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_16_095025) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_16_095629) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -46,6 +46,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_16_095025) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
