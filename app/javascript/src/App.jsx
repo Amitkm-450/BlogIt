6cd99cce5ded94b436a9c3476e7a6b1abc28ff3c
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-16 13:00:39 +0530

    Added Post show component

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -9,12 +9,14 @@ import {
 
 import Home from "./components/Home";
 import CreatePost from "./components/Post/Create";
+import ShowPost from "./components/Post/Show";
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact component={CreatePost} path="/posts/new" />
       <Route exact component={Home} path="/posts" />
+      <Route exact component={CreatePost} path="/posts/new" />
+      <Route exact component={ShowPost} path="/posts/:slug" />
       <Route exact path="/about" render={() => <div>About</div>} />
       <Redirect exact from="/" to="/posts" />
     </Switch>
--- app/javascript/src/components/Post/Card.jsx
+++ app/javascript/src/components/Post/Card.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { Typography } from "@bigbinary/neetoui";
 import dayjs from "dayjs";
+import { useHistory } from "react-router-dom";
 
-const Card = ({ title, description, created_at }) => {
+const Card = ({ title, description, created_at, slug }) => {
   const creationDate = dayjs(created_at).format("D MMMM YYYY");
+  const history = useHistory();
 
   return (
-    <div className="flex justify-between border-b p-4">
+    <div
+      className="flex cursor-default justify-between border-b p-4"
+      onClick={() => history.push(`/posts/${slug}`)}
+    >
       <div className="flex flex-col gap-y-1">
         <div className="flex gap-x-2">
           <Typography style="h3" weight="semibold">
