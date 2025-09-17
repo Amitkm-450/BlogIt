f01043341cea9f992ed5243e2424849550b0f163
Amit Kumar Mohapatra <130365583+Amitkm-450@users.noreply.github.com>
2025-09-17 12:09:22 +0530

    Implemented category sidebar (#16)
    * Added category sidebar component
    
    * Added add category modal

---
+++ app/javascript/src/components/commons/Category/index.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+
+import { Plus, Search } from "@bigbinary/neeto-icons";
+import { Input, Button, Spinner, Typography } from "@bigbinary/neetoui";
+import classNames from "classnames";
+import Logger from "js-logger";
+import { includes, without, append } from "ramda";
+import { useTranslation } from "react-i18next";
+
+import AddCategoryModel from "./AddCategoryModal";
+
+import categoriesApi from "../../../apis/categories";
+
+const Sidebar = ({ isCategorySidebarOpen }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchCategories = async () => {
+      try {
+        const categories = await categoriesApi.fetch();
+        setCategories(categories);
+      } catch (error) {
+        Logger.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  const handleSelectedCategory = ({ name }) => {
+    setSelectedCategories(prevSelected =>
+      includes(name, prevSelected)
+        ? without([name], prevSelected)
+        : append(name, prevSelected)
+    );
+  };
+
+  const filteredCategories = categories.filter(category =>
+    category.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  if (loading) {
+    return (
+      <div
+        className={classNames("h-full w-auto flex-col border-r p-4", {
+          flex: isCategorySidebarOpen,
+          hidden: !isCategorySidebarOpen,
+        })}
+      >
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={classNames("h-full w-auto flex-col border-r bg-gray-100 p-4", {
+        flex: isCategorySidebarOpen,
+        hidden: !isCategorySidebarOpen,
+      })}
+    >
+      <div className="flex items-center justify-between border-b pb-2">
+        <Typography className="text-lg font-semibold" style="h2">
+          {t("categorySidebar.header")}
+        </Typography>
+        <Button
+          icon={Plus}
+          style="icon"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        />
+      </div>
+      <div className="mt-4">
+        <Input
+          placeholder={t("categorySidebar.searchInput.placeholder")}
+          prefix={<Search size={16} />}
+          value={searchTerm}
+          onChange={event => setSearchTerm(event.target.value)}
+        />
+      </div>
+      <div className="mt-4">
+        <Input
+          readOnly
+          placeholder={t("categorySidebar.selectedCategories.placeholder")}
+          value={selectedCategories.join(", ")}
+        />
+      </div>
+      <ul className="mt-4 space-y-2">
+        {filteredCategories.map(category => (
+          <li
+            key={category.id}
+            className={`cursor-pointer rounded p-2 text-gray-700 shadow-sm hover:shadow-lg ${
+              selectedCategories?.includes(category.name)
+                ? "bg-green-400 text-white"
+                : "bg-white"
+            }`}
+            onClick={() => {
+              handleSelectedCategory(category);
+            }}
+          >
+            {category.name}
+          </li>
+        ))}
+      </ul>
+      <AddCategoryModel
+        {...{ isModalOpen }}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
--- app/javascript/src/components/commons/Sidebar.jsx
+++ app/javascript/src/components/commons/Sidebar.jsx
@@ -1,35 +1,49 @@
 import React from "react";
 
-import { Book, Edit, List, User } from "@bigbinary/neeto-icons";
+import { Book, Edit, List, MenuLayout, User } from "@bigbinary/neeto-icons";
 import { Button } from "@bigbinary/neetoui";
 import { useTranslation } from "react-i18next";
 
-const Sidebar = () => {
+const Sidebar = ({ setIsCategorySidebarOpen }) => {
   const { t } = useTranslation();
 
   return (
     <div className="flex h-screen w-24 flex-col items-center gap-1 border-r-2 border-gray-200 px-1 py-2">
-      <Button icon={Book} size="large" style="tertiary" to="/posts" />
-      <Button
-        icon={List}
-        size="large"
-        style="secondary"
-        to="/posts"
-        tooltipProps={{
-          content: t("sidebar.postsList"),
-          position: "right",
-        }}
-      />
-      <Button
-        icon={Edit}
-        size="large"
-        style="secondary"
-        to="/posts/new"
-        tooltipProps={{
-          content: t("sidebar.postCreate"),
-          position: "right",
-        }}
-      />
+      <div className="flex flex-col gap-y-4">
+        <Button icon={Book} size="large" style="tertiary" to="/posts" />
+        <Button
+          icon={List}
+          size="large"
+          style="secondary"
+          to="/posts"
+          tooltipProps={{
+            content: t("sidebar.postsList"),
+            position: "right",
+          }}
+        />
+        <Button
+          icon={Edit}
+          size="large"
+          style="secondary"
+          to="/posts/new"
+          tooltipProps={{
+            content: t("sidebar.postCreate"),
+            position: "right",
+          }}
+        />
+        <Button
+          icon={MenuLayout}
+          size="large"
+          style="secondary"
+          tooltipProps={{
+            content: t("sidebar.category"),
+            position: "right",
+          }}
+          onClick={() => {
+            setIsCategorySidebarOpen(prev => !prev);
+          }}
+        />
+      </div>
       <Button
         className="mt-auto"
         icon={User}
