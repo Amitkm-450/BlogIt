349473ba85719c142a6fcf0639ac712c09556f45
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-17 10:36:03 +0530

    Added category sidebar component

--- app/javascript/src/components/commons/PageLayout.jsx
+++ app/javascript/src/components/commons/PageLayout.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
+import CategorySidebar from "./Category";
 import Sidebar from "./Sidebar";
 
-const PageLayout = ({ children }) => (
-  <div className="flex h-screen w-screen">
-    <div className="flex-shrink-0">
-      <Sidebar />
+const PageLayout = ({ children }) => {
+  const [isCategorySidebarOpen, setIsCategorySidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="flex-shrink-0">
+        <Sidebar {...{ setIsCategorySidebarOpen }} />
+      </div>
+      {/* <div className="h-screen w-4 bg-red-400">Sidebar2</div> */}
+      <CategorySidebar {...{ isCategorySidebarOpen }} />
+      <div className="flex-grow p-6">{children}</div>
     </div>
-    <div className="flex-grow p-6">{children}</div>
-  </div>
-);
+  );
+};
 
 export default PageLayout;
