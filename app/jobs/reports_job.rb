69a5d32a11881492c811745bbc24de619e16deeb
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-21 22:42:12 +0530

    Integrated Active storage for pdf download

--- app/jobs/reports_job.rb
+++ app/jobs/reports_job.rb
@@ -3,7 +3,7 @@
 class ReportsJob
   include Sidekiq::Job
 
-  def perform(post_id, report_path)
+  def perform(post_id)
     post = Post.find(post_id)
     content = ApplicationController.render(
       assigns: {
@@ -13,8 +13,12 @@ class ReportsJob
       layout: "pdf"
     )
     pdf_blob = WickedPdf.new.pdf_from_string content
-    File.open(report_path, "wb") do |f|
-      f.write(pdf_blob)
+    if post.report.attached?
+      post.report.purge_later
     end
+    post.report.attach(
+      io: StringIO.new(pdf_blob), filename: "#{post.slug}_report.pdf",
+      content_type: "application/pdf")
+    post.save
   end
 end
