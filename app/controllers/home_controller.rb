2cb01b415974402a6725df4dcac32745d4d77884
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-26 11:53:44 +0530

    Added seed data for organizations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user_using_x_auth_token
 
   def index
-    render
+    respond_to do |format|
+      format.html
+      format.json { render json: { status: "ok" } }
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Organization.find_or_create_by!(name: "Pixel Compute") do |org|
+  org.description = "This is the default organization"
+end
