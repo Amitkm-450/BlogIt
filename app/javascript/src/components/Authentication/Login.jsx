ea6623ac55658e57594e5855593368925269f2bd
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-11-07 00:30:41 +0530

    Started blogit refactoring

--- app/javascript/src/components/Authentication/Login.jsx
+++ app/javascript/src/components/Authentication/Login.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 import LoginForm from "components/Authentication/Form/Login";
 import { useLogin } from "hooks/reactQuery/useUsersApi";
-import Logger from "js-logger";
 
 const Login = () => {
   const { mutate: login } = useLogin();
 
   const handleSubmit = values => {
     login(values, {
-      onSuccess: async () => {
-        Logger.log("onSuccess per call 1");
-        await new Promise(r => setTimeout(r, 0));
+      onSuccess: () => {
         window.location.href = "/posts";
-        Logger.log("onSuccess per call 2");
       },
     });
   };
